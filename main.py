f66ba991906f790f38360c07eed2ad10aa1ecced
hujinming <hujinming090722@outlook.com>
2025-10-18 20:06:22 +0800

    finally!!! 完成了部分期望功能，明天或今天会对代码进行重构

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ ButtonOfExit.grid(row=4,column=0,sticky=E)
 
 
 def ExamStart():
+    start_time_input = EntryStartTime.get()  # 保存开始时间输入值
+    minutes_input = EntryMinutes.get()       # 保存时长输入值
+    subject_input = EntrySub.get()           # 保存科目输入值
+    subject_text = f"本场考试的科目是{subject_input}" if subject_input else "未输入考试科目"
+    print(subject_input)
+    
     
     startTimeALL=datetime.strptime(EntryStartTime.get(),"%H:%M")#获取输入并转换为'datetime.datetime
     time_interval1 = timedelta(minutes=int(EntryMinutes.get()))#将用户输入考试时长的数字转换为时间
@@ -42,8 +48,6 @@ def ExamStart():
             else:
                 timeLABEL.config(text="考试结束！")
 
-#MD，Windowsapp远程桌面太难用了，输入法不好切而且还无法把键盘直接传入，Fu** microsoft
-
     Testseconds = int(time_interval1.total_seconds())#将用户输入的分钟转为秒(浮点数形式)
     print(type(startTimeALL)) #调试时瞎写的
     print(startTimeALL)
@@ -52,29 +56,36 @@ def ExamStart():
     root.destroy() #可以在root窗口下计算的工作完成之后关闭root
     countDownWindow=Tk()
     now = datetime.now()    #获取datetime格式的现在的时间
+    
+    
     if now.time() < startTime:#当开始时间比现在小（还未开始）
         print("a")
         today=now.date()
         startTimeplusdate=datetime.combine(today,startTime)
         
-        timeLABEL=Label(countDownWindow,text="Test Not start",font=("TkDefaultFont",64))
+        timeLABEL=Label(countDownWindow,text=f"考试将在{start_time_input}开始",font=("TkDefaultFont",64))
         timeLABEL.grid(row=0,column=0)
         today = now.date()
         start_datetime = datetime.combine(today, startTime)  # 组合成完整 datetime
         current_datetime = now  # 现在的完整 datetime
         wait_seconds = (start_datetime - current_datetime).total_seconds()
         countDownWindow.after(int(wait_seconds * 1000), count_down, Testseconds)
+        subjectlabel=Label(countDownWindow,text=f"{subject_text}",font=("TkDefaultFont",64))
+        subjectlabel.grid(row=1,column=0)
         #if now.time() == startTime  
-    elif now.time() == startTime:#当考试进行中
+    elif now.time() == startTime or now.time() > startTime < endTime :#当考试进行中
         print("b")
         # 更新标签显示
         timeLABEL=Label(countDownWindow,text="考试结束！",font=("TkDefaultFont",64))
         timeLABEL.grid(row=0,column=0)
         
         REMSEC= int(Testseconds)
-        
+        subjectlabel=Label(countDownWindow,text=f"{subject_text}",font=("TkDefaultFont",64))
         count_down(REMSEC)
         timeLABEL.grid(row=0,column=0)
+        subjectlabel.grid(row=1,column=0)
+        
+        
         
 
 
